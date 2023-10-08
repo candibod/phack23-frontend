e458fe19c28dd10da5789760c4d18134e4885a18
Jeevan Reddy <reddyjeevan95@gmail.com>
2023-10-08 10:49:44 -0400

    + Netlify deploy issue fix

--- src/modules/views/AppFooter.js
+++ src/modules/views/AppFooter.js
@@ -1,42 +1,7 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
 import Container from "@mui/material/Container";
 import Typography from "../components/Typography";
-import TextField from "../components/TextField";
-
-function Copyright() {
-  return (
-    <React.Fragment>
-      {"© "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-    </React.Fragment>
-  );
-}
-
-const iconStyle = {
-  width: 48,
-  height: 48,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  mr: 1,
-};
-
-const LANGUAGES = [
-  {
-    code: "en-US",
-    name: "English",
-  },
-  {
-    code: "fr-FR",
-    name: "Français",
-  },
-];
 
 export default function AppFooter() {
   return (
